7efd93303a79b311c1146968c479510c374c1860
NianShaoSix <13979696073@163.com>
2025-02-01 10:11:18 +0800

    updata

---
+++ bite_study/list/list_1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int main(){
+    //int ch = getchar();
+    //putchar(ch);
+    //printf("%*.*f",6,2,0.5);
+    return 0;
+}
--- c++/contributetest.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-int main()
-{
-    std::cout << "Hello World!";
-    return 0;
-}//这是一次对contribute的测试
