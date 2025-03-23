0ad837f9555049854b56fbf7433d8c1cdc8a2c3d
NianShaoSix <13979696073@163.com>
2025-03-23 23:25:35 +0800

    test

--- c++/c++_review/cout.cpp
+++ c++/c++_review/cout.cpp
@@ -1,23 +1,35 @@
+// #include <iostream>
+// #include <iomanip>
+// using namespace std;
+// int main() {
+//     //int a = 123;
+//     //cout << setw(10) << setfill('*') << a << endl;
+//     //return 0;
+//     //double pi = 3.14182653589793;
+//     //cout << pi << endl;
+//     //cout << fixed << pi << endl;
+//     //cout << scientific << pi << endl;
+//     //cout << fixed << setprecision(8) << pi << endl;
+//     //int n = 255;
+//     //cout << n << endl;
+//     //cout << dec << n << endl;//十进制
+//     //cout << hex << n << endl;//十六进制
+//     //cout << oct << n <<endl;//八进制
+//     int a = 123;
+//     cout << a << endl;
+//     cout << setw(10) << a << endl;
+//     cout << setw(10) << left << a << endl;
+//     return 0;
+// }
 #include <iostream>
-#include <iomanip>
-using namespace std;
+#include <string>
+
 int main() {
-    //int a = 123;
-    //cout << setw(10) << setfill('*') << a << endl;
-    //return 0;
-    //double pi = 3.14182653589793;
-    //cout << pi << endl;
-    //cout << fixed << pi << endl;
-    //cout << scientific << pi << endl;
-    //cout << fixed << setprecision(8) << pi << endl;
-    //int n = 255;
-    //cout << n << endl;
-    //cout << dec << n << endl;//十进制
-    //cout << hex << n << endl;//十六进制
-    //cout << oct << n <<endl;//八进制
-    int a = 123;
-    cout << a << endl;
-    cout << setw(10) << a << endl;
-    cout << setw(10) << left << a << endl;
+    std::string str = "Hello";
+    str += " ";  // 追加一个空格
+    str += "World";  // 追加另一个字符串
+    str += '!';  // 追加一个字符
+
+    std::cout << str << std::endl;  // 输出: "Hello World!"
     return 0;
 }
