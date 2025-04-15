85ba9035e4aefdbd072def72d245dd4b7860fd71
NianShaoSix <13979696073@163.com>
2025-04-15 15:17:00 +0800

    列表学习

---
+++ python/pystudy/列表/list.py
@@ -0,0 +1,47 @@
+#列表函数
+# len(list)               求列表元素个数
+# max(list)               返回列表最大值
+# min(list)               返回列表元素最小值
+# list(seq)               将元组转换为列表
+# list.append(obj)        在列表末尾添加新的对象
+# list.count(obj)         统计某个元素在列表中出现的次数
+# list.extend(seq)        在列表末尾一次性追加另一个序列的多个值
+# list.index(obj)         从列表中找出某个值第一个匹配项的索引位置
+# list.insert(index,obj)  将对象插入列表
+# list.pop(obj=lsit[-1])  移除列表中的一个元素（默认为最后一个元素）
+# list.remove(obj)        移除列表中的一个元素（参数是列表中元素）
+# list.reverse()          反向列表中元素
+# list.sort([func])       对原列表进行排序
+
+
+
+
+
+
+
+#列表学习
+list1 = ['lisi1','lisi2','lisi3','lisi4']
+list2 = ['lisi',14,'geade3']
+# print(list1)
+# print(list2)
+# print(list1[2])
+# print(list2[0:2])
+
+# list1[0] = 'lisi'
+# print(list1)
+# list1.append('lisi5')
+# print(list1)
+# del list1[0]
+# print(list1)
+# print(len(list1))
+# list1[0] = 'lisi'
+# list3 = list1+list2
+# list4 = list1*3
+# print(list3)
+# print(list4)
+# print('lisi1' in list1)
+# for i in list1:
+#     print(i,end=' ')
+
+
+
